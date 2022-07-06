353a2dc00e56fb1ad86ee5af9af1281b944a43c8
shardoolsingh <shardoolsingh4@gmail.com>
2022-07-07 03:52:02 +0530

    Has done day 1 exercises

---
+++ 01_Day_Introduction/01_day_starter/exercises.js
@@ -0,0 +1,45 @@
+console.log("This is for the exercises");
+
+//comments can make code readable
+
+//Welcome to 30 days of JS
+
+/*Comments can make code:
+-readable
+-easy to use
+-and informative
+*/
+
+//Declaring Variables
+
+let formalName = "Shardool Singh",
+    realAge = 25,
+    isHeMarried = false,
+    future = undefined,
+    money = null;
+
+console.log(typeof(formalName));
+console.log(typeof(realAge));
+console.log(typeof(isHeMarried));
+console.log(typeof(future));
+console.log(typeof(money));
+
+//Declaring variable without assigning values
+let dayOne, dayTwo;
+
+let isSunday,
+    isNewYear;
+
+console.log(typeof(isSunday));
+console.log(typeof(isNewYear));
+console.log(typeof(dayOne));
+
+//My Age and your Age
+let playMyAge,
+    playYourAge;
+
+playMyAge = 25;
+playYourAge = 30;
+
+console.log("I am " + playMyAge + " years old.");
+console.log("You are " + playYourAge + " years old.");
