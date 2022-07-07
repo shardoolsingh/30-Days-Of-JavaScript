18f9b0ff618d518fd3c40c32807faf0f742d32ab
shardoolsingh <shardoolsingh4@gmail.com>
2022-07-07 10:40:01 +0530

    Working on lessons of Day 2

---
+++ 02_Day_Data_types/playbox/playbox.js
@@ -0,0 +1,56 @@
+
+// let string = "Hello";
+
+// function ifH(x){
+
+//     if(x === string[0]){
+//         console.log("The character entered is H.");
+//     } else{
+//         console.log("The character entered is not H.");
+//     }
+// }
+
+// ifH("H");
+
+//Arrays
+
+// let nums = [1, 2, 3];
+
+// console.log(nums);
+
+// nums[0] = 23;
+
+// console.log(nums[0]);
+
+//Function to print numbers from 1 to 100
+
+// // function printHundreds(){
+// //     let i;
+// //     for(i = 1; i <= 100; i++){
+// //         console.log(i);
+// //     }
+
+// //     if(i == 101){
+// //         console.log("Printed 1 to 100. Task finished.");
+// //         return 0;
+// //     }
+// // }
+
+// printHundreds();
+
+//Comparing non primitive data type
+
+// let nums1 = [1, 2, 3],
+//     nums2 = [1, 2, 3];
+
+// console.log(nums1 == nums2);    //false
+
+//Comparing by referen instead of value: convention
+
+// let nums1 = [1, 2, 3];
+// let nums2 = nums1;
+
+// console.log(nums1 == nums2);    //true
+
+
+
